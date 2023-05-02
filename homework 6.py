809c7016164e3cab863b82420f6953b36c73b9ce
SkySong4 <128451262+SkySong4@users.noreply.github.com>
2023-05-02 09:32:31 -0500

    Initial commit

---
+++ homework 6.py
@@ -0,0 +1,72 @@
+# PPHA 30537
+# Spring 2023
+# Homework 6
+
+# YOUR NAME HERE
+
+# YOUR CANVAS NAME HERE
+# YOUR GITHUB USER NAME HERE
+
+# Due date: Monday May 8th before midnight
+# Write your answers in the space between the questions, and commit/push only
+# this file to your repo. Note that there can be a difference between giving a
+# "minimally" right answer, and a really good answer, so it can pay to put
+# thought into your work.
+
+##################
+
+#NOTE: All of the plots the questions ask for should be saved and committed to
+# your repo under the name "q1_plot.png", "q2_plot.png", etc. If a question calls
+# for more than one plot, name them "q1a_plot", "q1b_plot", etc.
+
+# Question 1: With the x and y values below, create a plot using only Matplotlib.
+# You should plot y1 as a scatter plot and y2 as a line, using different colors
+# and a legend.  You can name the data simply "y1" and "y2".  Make sure the
+# axis labels are legible.  Add a title that reads "HW6 Q1".
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import os
+
+x = pd.date_range(start='1990/1/1', end='1991/12/1', freq='MS')
+y1 = np.random.normal(10, 2, len(x))
+y2 = [np.sin(v)+10 for v in range(len(x))]
+
+# Question 2: Using only Matplotlib, reproduce the figure in this repo named
+# question_2_figure.png.
+
+# Question 3: Load the mpg.csv file that is in this repo, and create a
+# plot that tests the following hypothesis: a car with an engine that has
+# a higher displacement (i.e. is bigger) will get worse gas mileage than
+# one that has a smaller displacement.  Test the same hypothesis for mpg
+# against horsepower and weight.
+
+# Question 4: Continuing with the data from question 3, create a scatter plot 
+# with mpg on the y-axis and cylinders on the x-axis.  Explain what is wrong 
+# with this plot with a 1-2 line comment.  Now create a box plot using Seaborn
+# that uses cylinders as the groupings on the x-axis, and mpg as the values
+# up the y-axis.
+
+# Question 5: Continuing with the data from question 3, create a two-by-two 
+# grid of subplots, where each one has mpg on the y-axis and one of 
+# displacement, horsepower, weight, and acceleration on the x-axis.  To clean 
+# up this plot:
+#   - Remove the y-axis tick labels (the values) on the right two subplots - 
+#     the scale of the ticks will already be aligned because the mpg values 
+#     are the same in all axis.  
+#   - Add a title to the figure (not the subplots) that reads "Changes in MPG"
+#   - Add a y-label to the figure (not the subplots) that says "mpg"
+#   - Add an x-label to each subplot for the x values
+# Finally, use the savefig method to save this figure to your repo.  If any
+# labels or values overlap other chart elements, go back and adjust spacing.
+
+# Question 6: Are cars from the USA, Japan, or Europe the least fuel
+# efficient, on average?  Answer this with a plot and a one-line comment.
+
+# Question 7: Using Seaborn, create a scatter plot of mpg versus displacement,
+# while showing dots as different colors depending on the country of origin.
+# Explain in a one-line comment what this plot says about the results of 
+# question 6.
+
